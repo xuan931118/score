bcbdfe6f1c9fa9464c29b6159afae89e7f5172a3
xuan931118 <mdlliu1118@gmail.com>
2025-05-15 11:19:12 +0800

    add max min

--- score (1).py
+++ score (1).py
@@ -9,7 +9,15 @@ Original file is located at
 
 score = input().split()
 count = 0
+min,max = int(score[0]),int(score[0])
 for i in score:
   if int(i) < 60:
     count = count + 1
 print("不及格人數：",count)
+  if int(i) < min:
+    min = int(i)
+  if int(i) > max:
+    max = int(i)
+print("不及格人數：",count)
+print("min：",min)
+print("max：",max)
